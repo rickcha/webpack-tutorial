b6273fa9fdd342892ea547252d5aa2d003b6e7bc
Rick Cha <rick.cha@outlook.com>
2020-06-27 19:43:56 -0700

    AssetManagement

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,18 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         use: ["file-loader"],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"],
+      },
     ],
   },
 };
